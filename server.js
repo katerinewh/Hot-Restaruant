d3bc5cf08cd696d327e669f5b59e34d7163b71aa
Katerine <katerinehw@att.net>
2018-12-14 13:41:40 -0800

    debug code-links to homepage

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, "add.html"));
   });
   
-  app.get("/all", function(req, res) {
+  app.get("/viewTables", function(req, res) {
     res.sendFile(path.join(__dirname, "viewTables.html"));
   });
   
